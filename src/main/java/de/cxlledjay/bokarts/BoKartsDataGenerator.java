a15cab0c7302630a44addde67fceb6a7e18fd7ad
cxlledjay <cxlledj@gmx.de>
2026-09-11 12:37:32 +0200

    initial commit

---
+++ src/main/java/de/cxlledjay/bokarts/BoKartsDataGenerator.java
@@ -0,0 +1,11 @@
+package de.cxlledjay.bokarts;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BoKartsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
