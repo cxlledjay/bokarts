dc8b066b7d32ca7c2cd397dbd7513d591a46c5f5
cxlledjay <cxlledj@gmx.de>
2026-09-11 12:46:32 +0200

    initial setup

---
+++ src/main/java/de/cxlledjay/bokarts/BoKartsClient.java
@@ -0,0 +1,10 @@
+package de.cxlledjay.bokarts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BoKartsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
