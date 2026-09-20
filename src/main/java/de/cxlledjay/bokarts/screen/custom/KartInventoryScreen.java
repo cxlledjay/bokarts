a73667f0b71f8770a96b2f51d8c17bb49f598af5
cxlledjay <cxlledj@gmx.de>
2026-09-20 12:31:19 +0200

    kart rendering in inventory screen

--- src/main/java/de/cxlledjay/bokarts/screen/custom/KartInventoryScreen.java
+++ src/main/java/de/cxlledjay/bokarts/screen/custom/KartInventoryScreen.java
@@ -1,5 +1,6 @@
 package de.cxlledjay.bokarts.screen.custom;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import de.cxlledjay.bokarts.BoKarts;
 import de.cxlledjay.bokarts.entity.custom.KartEntity;
 import de.cxlledjay.bokarts.networking.packet.AddFuelPayloadC2S;
@@ -7,13 +8,19 @@ import de.cxlledjay.bokarts.networking.packet.SetHornPayloadC2S;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ButtonTextures;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.joml.Quaternionf;
 
 
 @Environment(EnvType.CLIENT)
@@ -108,9 +115,12 @@ public class KartInventoryScreen extends HandledScreen<KartInventoryScreenHandle
 
     @Override
     protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
-        context.drawTexture(BACKGROUND_TEXTURE, this.x, this.y, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        int i = (this.width - this.backgroundWidth) / 2;
+        int j = (this.height - this.backgroundHeight) / 2;
+        context.drawTexture(BACKGROUND_TEXTURE, i, j, 0, 0, this.backgroundWidth, this.backgroundHeight);
 
         // TODO: render kart like horses
+        this.drawKart(context, i + 52, j + 52, 21, mouseX, mouseY, this.kart);
     }
 
     @Override
@@ -126,8 +136,8 @@ public class KartInventoryScreen extends HandledScreen<KartInventoryScreenHandle
         String hornString = "";
         int fuelColor = 0xB71C1C; //red
         if(this.kart != null){
-            fuelCapacityString = this.kart.getFormattedFuelCapacityString(this.kart.currentFuel);
-            odoString = this.kart.getFormattedDistanceString(this.kart.currentOdometer);
+            fuelCapacityString = KartEntity.getFormattedFuelCapacityString(this.kart.currentFuel);
+            odoString = KartEntity.getFormattedDistanceString(this.kart.currentOdometer);
             hornString = "[" + (this.kart.getHornSound().ordinal()+1) + "/10]";
 
             float fuelCapacityPercentage = this.kart.currentFuel / KartEntity.FUEL_TANK_MAX_CAPACITY;
@@ -142,5 +152,48 @@ public class KartInventoryScreen extends HandledScreen<KartInventoryScreenHandle
         context.drawText(this.textRenderer, fuelCapacityString  , this.x + 102, this.y + 23,        fuelColor, false);
         context.drawText(this.textRenderer, odoString           , this.x + 102, this.y + 23 + 18,   0xE29700, false);
         context.drawText(this.textRenderer, hornString   ,       this.x + 102, this.y + 23 + 36,    0x3E349E, false);
+    }
+
+
+
+    // helper method to draw kart following the mouse
+    private void drawKart(DrawContext context, int x, int y, int size, float mouseX, float mouseY, KartEntity kart) {
+        // 1. Swap the subtraction order to fix the inverted tracking
+        float lookX = (float)Math.atan((mouseX - x) / 40.0f);
+        float lookY = (float)Math.atan((mouseY - y) / 40.0f);
+
+        MatrixStack matrices = context.getMatrices();
+        matrices.push();
+
+        matrices.translate(x, y, 50.0);
+        matrices.scale((float)size, (float)size, (float)-size);
+
+        Quaternionf rotation = new Quaternionf().rotateZ((float)Math.PI);
+
+        // 2. Adjust Pitch (Up/Down)
+        // If up/down feels inverted to you as well, just remove the negative sign here:
+        rotation.rotateX(-lookY * 20.0F * ((float)Math.PI / 180F));
+
+        // 3. Change 135.0F to 180.0F so the kart faces straight out of the screen
+        rotation.rotateY((lookX * 40.0F + 180.0F) * ((float)Math.PI / 180F));
+
+        matrices.multiply(rotation);
+
+        // 4. Set up the Render Dispatcher
+        EntityRenderDispatcher dispatcher = MinecraftClient.getInstance().getEntityRenderDispatcher();
+        dispatcher.setRenderShadows(false); // No ground shadow in the UI
+
+        // 5. Render the entity
+        RenderSystem.runAsFancy(() -> {
+            // 0xF000F0 is the magic number for "Full Brightness" in the lighting map
+            dispatcher.render(kart, 0.0, 0.0, 0.0, 0.0F, 1.0F, matrices, context.getVertexConsumers(), 0xF000F0);
+        });
+
+        // 6. Flush the buffers to draw to the screen
+        context.draw();
+
+        // 7. Cleanup
+        dispatcher.setRenderShadows(true);
+        matrices.pop();
     }
 }
